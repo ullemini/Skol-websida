f3f60c93d792d3583ec84f74bb6d05361b6305dd
ullemini <ullesone@gmail.com>
2024-11-18 12:24:00 +0100

    Intermediete save

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,39 @@
-<main>
+<script>
+    let color = "black"
+    let colors = [
+        {namn: "Blå", value: "blue"},
+        {namn: "Röd", value: "red"},
+        {namn: "Gul", value: "yellow"},
+        {namn:"Grön", value: "green"},
+        {namn: "vit", value: "white"},
+        {namn:"svart",value: "black"},
+        {namn:"gray",value: "gray"},
+        {namn:"grey",value: "grey"},
+        ]
 
+    bind:value={color}
+    function handleSubmit(){
+        alert("Välkommen")
+    }
+</script>
+<main>
+    <div class="container">
+        <h1>Registrering</h1>
+        <form class="input_feald" on:submit|preventDefault={handleSubmit}>
+            <label for="name">Namn:</label>
+            <input type="text" id="name" >
+            <label for="login">login</label>
+            <input type="password" id="login">
+            <label for="Favoritfärg">Favoritfärg</label>
+            <select name="Favoritfärg" id="Favoritfärg">
+                {#each colors as c}
+                    <option value={c.color}>{c.namn}</option>
+                {/each}
+            </select>
+            <div style="width: 100px; height: 100px; border-radius: 50%; overflow:hidden; background-color:{color};"></div>
+            <input type="submit" value="Registrera" style="color: white;">
+        </form>
+    </div>
 </main>
 
 <style>
@@ -11,6 +45,10 @@
         padding: 5%;
     }   
     .container{
+        display:flex ;
+        flex-direction:column;
+        align-items: center;
+        justify-content: space-evenly;
         border: solid 5px rosybrown;
         border-radius: 10px;
         width: 35%;
@@ -18,5 +56,11 @@
         background-color: #3a2e3b;
         margin: auto;
     
+    }
+    .input_feald input{
+        color: black;
+    }
+    .input_feald option{
+        color: black;
     }
 </style>
