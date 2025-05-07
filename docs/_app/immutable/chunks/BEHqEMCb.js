9f378d00ac6c4a651bfbf1d186d25f5dcf0e0451
ullemini <ullesone@gmail.com>
2025-05-07 11:52:04 +0200

    blog fix byld

---
+++ docs/_app/immutable/chunks/BEHqEMCb.js
@@ -0,0 +1,27 @@
+import{s as I,n as _}from"./DcZ3m06h.js";import{S as P,i as C,d as l,a,c as s,l as k,g as T,f as w,h as m,j as W,t as x}from"./DMnCdVkp.js";function U(z){let n,S="Den första bloggen",r,i,j=`Jag har kollat upp olika sätt att spela upp ljud i en browser.
+Jag hittade flera olika sidor som beskrev hur man gorde aoudio i html:`,u,d,p,f,h,b,g,o,y=`Till slut hittade jag
+<a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API" rel="nofollow">“Web Audio API”</a>
+som lädde mig Till artikeln
+<a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API" rel="nofollow">“Using the Web Audio API”</a>`,v,c,A;return{c(){n=m("h1"),n.textContent=S,r=W(),i=m("p"),i.textContent=j,u=W(),d=m("br"),p=x(`
+"HTML Audio Tag: Syntax, Usage, and Examples" från mimo [link](https://mimo.org/glossary/html/audio-tag)
+`),f=m("br"),h=x(`
+The Embed Audio elementfrån mozilla [link](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio)
+`),b=m("br"),g=x(`
+Att bara använda aoudio html tagen för att spela ljud upp skulle inte fungera för mig eftersom
+`),o=m("p"),o.innerHTML=y,v=W(),c=m("br"),A=x(`
+Jag tästatde ochså att använda Aoudio() i javascript med
+[tutorial](https://www.youtube.com/watch?v=YtrSp08rzxc)
+och en väldigt informativ 
+[tutorial 2](https://www.youtube.com/watch?v=VXWvfrmpapI)
+som jag inte har hunnigt se mer än 10 min på`)},l(t){n=s(t,"H1",{"data-svelte-h":!0}),k(n)!=="svelte-95duaz"&&(n.textContent=S),r=T(t),i=s(t,"P",{"data-svelte-h":!0}),k(i)!=="svelte-1a7bqdq"&&(i.textContent=j),u=T(t),d=s(t,"BR",{}),p=w(t,`
+"HTML Audio Tag: Syntax, Usage, and Examples" från mimo [link](https://mimo.org/glossary/html/audio-tag)
+`),f=s(t,"BR",{}),h=w(t,`
+The Embed Audio elementfrån mozilla [link](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio)
+`),b=s(t,"BR",{}),g=w(t,`
+Att bara använda aoudio html tagen för att spela ljud upp skulle inte fungera för mig eftersom
+`),o=s(t,"P",{"data-svelte-h":!0}),k(o)!=="svelte-k8l2k7"&&(o.innerHTML=y),v=T(t),c=s(t,"BR",{}),A=w(t,`
+Jag tästatde ochså att använda Aoudio() i javascript med
+[tutorial](https://www.youtube.com/watch?v=YtrSp08rzxc)
+och en väldigt informativ 
+[tutorial 2](https://www.youtube.com/watch?v=VXWvfrmpapI)
+som jag inte har hunnigt se mer än 10 min på`)},m(t,e){a(t,n,e),a(t,r,e),a(t,i,e),a(t,u,e),a(t,d,e),a(t,p,e),a(t,f,e),a(t,h,e),a(t,b,e),a(t,g,e),a(t,o,e),a(t,v,e),a(t,c,e),a(t,A,e)},p:_,i:_,o:_,d(t){t&&(l(n),l(r),l(i),l(u),l(d),l(p),l(f),l(h),l(b),l(g),l(o),l(v),l(c),l(A))}}}const H={title:"blog 1",date:"2025-04-09"},{title:M,date:B}=H;class J extends P{constructor(n){super(),C(this,n,null,U,I,{})}}export{J as default,H as metadata};
---
+++ docs/_app/immutable/entry/start.oWeFiFiB.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/DBlDg4Fg.js";export{a as start};
